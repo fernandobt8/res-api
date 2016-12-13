2464ef432b860e1e7e368a5e9fdc821328d81fa8
burigo <fernandobt8@gmail.com>
2016-12-13 10:07:27 -0200

    delete arquivos usados na requisição antiga

--- src/main/java/gov/nist/registry/common2/exception/xsd/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://exception.common2.registry.nist.gov/xsd")
-package gov.nist.registry.common2.exception.xsd;
--- src/main/java/gov/nist/registry/common2/registry/xsd/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://registry.common2.registry.nist.gov/xsd", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package gov.nist.registry.common2.registry.xsd;
