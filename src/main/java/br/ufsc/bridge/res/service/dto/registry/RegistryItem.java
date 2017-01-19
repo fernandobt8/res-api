e201a7db84a022ab100f31de8b7d2554d482a28f
evertonscoelho <evertonscoelho@gmail.com>
2017-01-19 13:58:15 -0200

    Testes respostas sem itens

--- src/main/java/br/ufsc/bridge/res/service/dto/registry/RegistryItem.java
+++ src/main/java/br/ufsc/bridge/res/service/dto/registry/RegistryItem.java
@@ -7,11 +7,13 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@ToString
 public class RegistryItem implements Serializable {
 	private static final long serialVersionUID = 1L;
 
