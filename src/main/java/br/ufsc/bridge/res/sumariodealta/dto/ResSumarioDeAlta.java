19c062cf264668e4cee6fbc8f7522015158e03e5
lucas <lucasjoao.lj@gmail.com>
2019-07-23 18:23:33 -0300

    Start structure to 'Sumário de alta'.

---
+++ src/main/java/br/ufsc/bridge/res/sumariodealta/dto/ResSumarioDeAlta.java
@@ -0,0 +1,10 @@
+package br.ufsc.bridge.res.sumariodealta.dto;
+
+import java.util.Date;
+
+public class ResSumarioDeAlta {
+
+	// XXX: tmp, aguardar exemplos
+	private Date dataAtendimentoDate;
+
+}
---
+++ src/main/java/br/ufsc/bridge/res/sumariodealta/write/builder/SumarioDeAltaBuilder.java
@@ -0,0 +1,19 @@
+package br.ufsc.bridge.res.sumariodealta.write.builder;
+
+import br.ufsc.bridge.res.dab.write.builder.base.ParentArquetypeWrapper;
+
+public class SumarioDeAltaBuilder extends ParentArquetypeWrapper<SumarioDeAltaBuilder> {
+
+	@Override
+	protected String openTags() {
+		// XXX aguardar exemplos
+		return null;
+	}
+
+	@Override
+	protected String closeTags() {
+		// XXX aguardar exemplos
+		return null;
+	}
+
+}
