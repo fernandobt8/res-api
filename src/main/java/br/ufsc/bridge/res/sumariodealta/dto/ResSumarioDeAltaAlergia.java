65969683b1061b43f9fcb3ad7488d49d7d06d81e
lucas <lucasjoao.lj@gmail.com>
2019-07-25 16:58:59 -0300

    Fix after merge with 400cbb8.

--- src/main/java/br/ufsc/bridge/res/sumariodealta/dto/ResSumarioDeAltaAlergia.java
+++ src/main/java/br/ufsc/bridge/res/sumariodealta/dto/ResSumarioDeAltaAlergia.java
@@ -5,20 +5,20 @@ import java.util.List;
 
 import javax.xml.xpath.XPathExpressionException;
 
-import br.ufsc.bridge.res.dab.domain.ResABGravidadeEnum;
+import br.ufsc.bridge.res.dab.domain.ResABCriticidadeEnum;
 import br.ufsc.bridge.soap.xpath.XPathFactoryAssist;
 
 public class ResSumarioDeAltaAlergia {
 
 	private String agente;
 	private String categoria;
-	private ResABGravidadeEnum gravidade;
+	private ResABCriticidadeEnum gravidade;
 	private List<ResSumarioAltaEventoReacao> eventoReacao = new ArrayList<>();
 
 	public ResSumarioDeAltaAlergia(XPathFactoryAssist xPathAlergia) throws XPathExpressionException {
 		this.agente = xPathAlergia.getString("./data/Agente_fslash_substância_específica/value/value");
 		this.categoria = xPathAlergia.getString("./data/Categoria_do_agente_causador_da_alergia_ou_reação_adversa/value/value");
-		this.gravidade = ResABGravidadeEnum.getByCodigo(xPathAlergia.getString("./data/Criticidade/value/defining_code/code_string"));
+		this.gravidade = ResABCriticidadeEnum.getByCodigo(xPathAlergia.getString("./data/Criticidade/value/defining_code/code_string"));
 
 		for (XPathFactoryAssist xPathEventoReacao : xPathAlergia.iterable(".//Evento_da_reação")) {
 			this.eventoReacao.add(new ResSumarioAltaEventoReacao(xPathEventoReacao));
