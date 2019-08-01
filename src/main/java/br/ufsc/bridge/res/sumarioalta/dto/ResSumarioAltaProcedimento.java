c55cd1a68e26deaf51126940d982e130bdd7891d
lucas <lucasjoao.lj@gmail.com>
2019-08-01 17:42:05 -0300

    New fields to 'procedimento' in 'sumarioAlta'.

--- src/main/java/br/ufsc/bridge/res/sumarioalta/dto/ResSumarioAltaProcedimento.java
+++ src/main/java/br/ufsc/bridge/res/sumarioalta/dto/ResSumarioAltaProcedimento.java
@@ -11,12 +11,24 @@ import br.ufsc.bridge.soap.xpath.XPathFactoryAssist;
 @Getter
 public class ResSumarioAltaProcedimento implements Serializable {
 
+	private static final String SIGTAP = "SIGTAP";
 	private static final long serialVersionUID = 1L;
 	private String nome;
 	private String codigo;
+	private String classificacao;
+	private String status;
 
 	public ResSumarioAltaProcedimento(XPathFactoryAssist xPathProcedimento) throws XPathExpressionException {
-		this.nome = xPathProcedimento.getString("./description/Procedimento_SUS/value/value");
-		this.codigo = xPathProcedimento.getString("./description/Procedimento_SUS/value/defining_code/code_string");
+		String startXPath = "./description/Procedimento_SUS/value";
+
+		this.nome = xPathProcedimento.getString(startXPath + "/value");
+		this.codigo = xPathProcedimento.getString(startXPath + "/defining_code/code_string");
+
+		this.classificacao = xPathProcedimento.getString(startXPath + "/defining_code/terminology_id/value");
+		if (this.classificacao != null && this.classificacao.contains(SIGTAP)) {
+			this.classificacao = SIGTAP;
+		}
+
+		this.status = xPathProcedimento.getString("./description/Status_do_procedimento/value/value");
 	}
 }
